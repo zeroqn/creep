7d8d51968c1d0d514373845be0d108d38f652e68
zero.qn <zero.qn@outlook.com>
2020-01-08 10:29:27 +0800

    Add Sync to Cloneable trait bounds

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@ use dyn_clone::DynClone;
 
 use std::{any::Any, collections::HashMap, fmt::Debug};
 
-pub trait Cloneable: DynClone + Send + Debug {
+pub trait Cloneable: DynClone + Send + Sync + Debug {
     fn as_any(&self) -> &dyn Any;
 }
 
 dyn_clone::clone_trait_object!(Cloneable);
 
-impl<T: DynClone + Send + Debug + 'static> Cloneable for T {
+impl<T: DynClone + Send + Sync + Debug + 'static> Cloneable for T {
     fn as_any(&self) -> &dyn Any {
         self
     }
